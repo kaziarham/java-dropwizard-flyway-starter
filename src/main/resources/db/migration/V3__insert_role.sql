f047d271543d4d6b1be3ce9f50198d10287a249a
Kazi Arham <kazi.arham@jpw04lrxv6.home>
2024-07-31 15:41:25 +0100

    Inserted new roles and user

---
+++ src/main/resources/db/migration/V3__insert_role.sql
@@ -0,0 +1,11 @@
+INSERT INTO role (title)
+VALUES ('Admin');
+
+INSERT INTO role (title)
+VALUES ('Employee');
+
+INSERT INTO user (username, password, role_id)
+VALUES ('Kazi', 'Abcd1234', 1);
+
+INSERT INTO user (username, password, role_id)
+VALUES ('Freddy', 'pwd1234?', 2);
